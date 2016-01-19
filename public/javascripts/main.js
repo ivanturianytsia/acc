8fac8fe431d7f155a883c6faa124bd0c720005dd
sirivan <vanyaturianitsa@gmail.com>
2016-01-19 23:58:12 +0100

    css

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,25 @@
+var help {
+    say: function(title, body) {
+        window.requestAnimationFrame(function() {
+            document.getElementById("loading").style.display = "inline";
+            document.getElementById("message-title").innerHTML = title;
+            document.getElementById("message-body").innerHTML = body;
+        });
+    }
+}
+
 var socket = io.connect();
+help.say("Loading...", "Please wait.");
 socket.on('connect', function() {
+    // help.say("Sorry.", "But your device do not support this feature.");
     socket.on('disconnect', function() {});
     socket.on('avalible', function(data) {
         if (data) {
             window.requestAnimationFrame(function() {
                 document.getElementById("loading").style.display = "none";
             });
+        } else {
+            help.say("Sorry.", "But another device is connected right now. Try later.");
         }
     });
 })
